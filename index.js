f97dc0c8faca6effced4c7be6af801f2f30e08cb
Mohammed Adnan Belim <115971540+adnanbelim@users.noreply.github.com>
2023-08-30 04:18:45 +0530

    Portfolio of Mohammed Adnan Belim

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const navbarToggle = document.getElementById('navbarToggle');
+const navList = document.getElementById('navList');
+
+navbarToggle.addEventListener('click', () => {
+    navList.classList.toggle('active');
+}).than(() => navbarToggle({
+    
+}))
+
